9da177bb2c79dca993dd4ef12a6989ab0c92b5e3
Ashutosh Dwivedi <ashutoshmzp1221@gmail.com>
2025-07-04 10:28:36 +0530

    Setup the room model

---
+++ server/models/Room.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const DrawingCommandSchema = new mongoose.Schema({
+    type: {
+        type: String,
+        required: true
+    },
+    data: {
+        type: Object,
+        required: true
+    },
+    timestamp: { 
+        type: Date, 
+        default: Date.now 
+    }
+});
+
+const RoomSchema = new mongoose.Schema({
+    roomId: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+        uppercase: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    lastActivity: {
+        type: Date,
+        default: Date.now
+    },
+    drawingData: [DrawingCommandSchema]
+});
+
+module.exports = mongoose.model('Room', RoomSchema);
